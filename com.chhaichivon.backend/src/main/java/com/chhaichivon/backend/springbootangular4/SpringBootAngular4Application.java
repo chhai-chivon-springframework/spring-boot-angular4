dc1613c205210f8b7ca1ca13be91a527b02f9186
chhaichivon <chivonchhai@hotmail.com>
2017-08-08 13:48:08 +0700

    spring-boot-angular4

---
+++ com.chhaichivon.backend/src/main/java/com/chhaichivon/backend/springbootangular4/SpringBootAngular4Application.java
@@ -0,0 +1,12 @@
+package com.chhaichivon.backend.springbootangular4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngular4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngular4Application.class, args);
+	}
+}
